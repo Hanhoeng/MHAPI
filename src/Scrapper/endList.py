137ab4eacea2f6d56b0239a81d30bd58bfc2cf36
Hiroshi <dgloria@tredicom.com>
2021-11-05 12:17:29 -0600

    chore: venv and some other things

---
+++ src/Scrapper/endList.py
@@ -0,0 +1,14 @@
+endpoints = [
+    "ailments",
+    "armor",
+    "armor/set",
+    "charms",
+    "decorations",
+    "events",
+    "items",
+    "locations",
+    "monsters",
+    "motion-values",
+    "skills",
+    "weapons",
+    ]
